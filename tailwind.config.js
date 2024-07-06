5ed74d3f8f8071b91c1433f757a40f8e5d7bc783
SANJOY <paulsanjoy2923@gmail.com>
2024-07-06 14:54:43 +0530

    Forgot to use github for my portfolio lol

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fontPop: ["Poppins", "Arial", "sans-serif"],
+        fontRal: ["Raleway", "Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
